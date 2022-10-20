bb60d377d9a8735e21c4fff63c641de998faebb2
Daniel Martinez <roanhawkeye@gmail.com>
2022-10-20 18:17:33 -0500

    [ruby-mentoring] - Design patterns task 2, add model and CRUD for users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :role, presence: true
+  validates :email, presence: true, length: { minimum: 8 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'welcome#index'
+  root 'users#index'
+
+  resources :users
 end
