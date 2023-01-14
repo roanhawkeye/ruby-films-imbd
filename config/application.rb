26c5e8a596f0715d1885e9f8a3408282684e1648
Daniel Martinez <roanhawkeye@gmail.com>
2023-01-14 13:40:26 -0600

    [ruby-mentoring] - Add rack middleware to filter ips and allow only permitted list of ip

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative "boot"
+require_relative "../lib/ip_security_filter"
 
 require "rails/all"
 
@@ -18,5 +19,6 @@ module FilmsImbd
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.use IpSecurityFilter
   end
 end
