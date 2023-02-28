1fffd387e2ae0fc81290389e6054ee29fedf122c
Daniel Martinez <roanhawkeye@gmail.com>
2023-02-28 15:32:07 -0600

    [ruby-mentoring-api] - Add admin role capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_045851) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_212151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_045851) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin_role", default: false
+    t.boolean "user_role", default: true
   end
 
 end
